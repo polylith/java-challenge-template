bb8bc40dc520d735869ce1ed85289fbae1a3e1b7
Jakob Blume <jakob@entwicklerheld.de>
2019-09-23 20:07:47 +0200

    made test sh executable and fixed test name

--- src/main/resources/archetype-resources/test_stage_1.sh
+++ src/main/resources/archetype-resources/test_stage_1.sh
@@ -3,7 +3,7 @@
 report_file="target/surefire-reports/TEST-${package}.${shortName}.challenge.stage1.ChallengeTest.xml"
 error_file="error.txt"
 
-mvn -q -Dtest=${package}.${shortName}.challenge.stage1.ExampleChallengeTest clean surefire-report:report > ${error_file} 2>&1
+mvn -q -Dtest=${package}.${shortName}.challenge.stage1.ChallengeTest clean surefire-report:report > ${error_file} 2>&1
 if [ -e ${report_file} ]
 then
     cat ${report_file}
